afdf696372d8f17cf91c4a76a653ec9acdcc1829
Rodolphe Souassi <87568171+Rodolphe27@users.noreply.github.com>
2023-07-14 10:30:03 +0200

    added a flag to detect fileshit

--- palim.c
+++ palim.c
@@ -6,8 +6,10 @@
 #include <pthread.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdbool.h>
 
-struct statistics {
+struct statistics
+{
     int lines;
     int lineHits;
     int files;
@@ -22,42 +24,47 @@ struct statistics {
 static struct statistics stats;
 
 // function declarations
-static void* processTree(void* path);
-static void* processDir(char* path);
-static void* processEntry(char* path, struct dirent* entry);
-static void* processFile(void* path);
+static void *processTree(void *path);
+static void *processDir(char *path);
+static void *processEntry(char *path, struct dirent *entry);
+static void *processFile(void *path);
 
 // Semaphore variables
-static SEM* statsMutex;
-static SEM* newDataSignal;
-static SEM* grepThreadsSem;
+static SEM *statsMutex;
+static SEM *newDataSignal;
+static SEM *grepThreadsSem;
 
 // Search string variable
-static char* search_string;
+static char *search_string;
 
-static void usage(void) {
+static void usage(void)
+{
     fprintf(stderr, "Usage: palim <string> <max-grep-threads> <trees...>\n");
     exit(EXIT_FAILURE);
 }
 
-static void die(const char* msg) {
+static void die(const char *msg)
+{
     perror(msg);
     exit(EXIT_FAILURE);
 }
 
-static void incStat(int* variable) {
+static void incStat(int *variable)
+{
     P(statsMutex);
     (*variable)++;
     V(statsMutex);
 }
 
-static void decStat(int* variable) {
+static void decStat(int *variable)
+{
     P(statsMutex);
     (*variable)--;
     V(statsMutex);
 }
 
-static void printStatistics() {
+static void printStatistics()
+{
     P(statsMutex);
     int lineHits = stats.lineHits;
     int lines = stats.lines;
@@ -70,27 +77,32 @@ static void printStatistics() {
     printf("%d/%d lines, %d/%d files, %d directories, %d active threads\r",
            lineHits, lines, fileHits, files, dirs, activeGrepThreads);
 
-    if (fflush(stdout) == EOF) {
+    if (fflush(stdout) == EOF)
+    {
         die("fflush");
     }
 }
 
-int main(int argc, char** argv) {
-    if (argc < 4) {
+int main(int argc, char **argv)
+{
+    if (argc < 4)
+    {
         usage();
     }
 
     // Convert argv[2] (<max-grep-threads>) into long with strtol()
     errno = 0;
-    char* endptr;
+    char *endptr;
     stats.maxGrepThreads = strtol(argv[2], &endptr, 10);
 
     // argv[2] cannot be converted into long without error
-    if (errno != 0 || endptr == argv[2] || *endptr != '\0') {
+    if (errno != 0 || endptr == argv[2] || *endptr != '\0')
+    {
         usage();
     }
 
-    if (stats.maxGrepThreads <= 0) {
+    if (stats.maxGrepThreads <= 0)
+    {
         fprintf(stderr, "max-grep-threads must not be negative or zero\n");
         usage();
     }
@@ -99,53 +111,66 @@ int main(int argc, char** argv) {
 
     statsMutex = semCreate(1);
 
-    if (statsMutex == NULL) {
+    if (statsMutex == NULL)
+    {
         die("semCreate");
     }
 
     newDataSignal = semCreate(0);
-    if (newDataSignal == NULL) {
+    if (newDataSignal == NULL)
+    {
         die("semCreate");
     }
 
     grepThreadsSem = semCreate(stats.maxGrepThreads);
-    if (grepThreadsSem == NULL) {
+    if (grepThreadsSem == NULL)
+    {
         die("semCreate");
     }
 
     pthread_t tids[argc - 3];
-    for (int i = 3; i < argc; i++) {
+    for (int i = 3; i < argc; i++)
+    {
         errno = pthread_create(&tids[i - 3], NULL, processTree, argv[i]);
-        if (errno != 0) {
+        if (errno != 0)
+        {
             die("pthread_create");
         }
 
         incStat(&stats.activeCrawlThreads);
 
         errno = pthread_detach(tids[i - 3]);
-        if (errno != 0) {
+        if (errno != 0)
+        {
             die("pthread_detach");
         }
     }
 
-    while (1) {
+    while (1)
+    {
+        P(newDataSignal);
         P(statsMutex);
         int crawlActiveNr = stats.activeCrawlThreads;
         int grepActiveNr = stats.activeGrepThreads;
         V(statsMutex);
 
-        if (crawlActiveNr > 0 || grepActiveNr > 0) {
+        if (crawlActiveNr > 0 || grepActiveNr > 0)
+        {
             printStatistics();
-        } else {
+        }
+        else
+        {
             break;
         }
     }
 
     printStatistics();
-    if (printf("\n") < 0) {
+    if (printf("\n") < 0)
+    {
         die("printf");
     }
-    if (fflush(stdout) == EOF) {
+    if (fflush(stdout) == EOF)
+    {
         die("fflush");
     }
 
@@ -156,51 +181,61 @@ int main(int argc, char** argv) {
     return EXIT_SUCCESS;
 }
 
-static void* processTree(void* path) {
-    char* treeStr = (char*)path;
+static void *processTree(void *path)
+{
+    char *treeStr = (char *)path;
     processDir(treeStr);
     decStat(&stats.activeCrawlThreads);
+    V(newDataSignal);
     printStatistics();
     return NULL;
 }
 
-static void* processDir(char* path) {
-    DIR* dirPointer = opendir(path);
-    if (!dirPointer) {
+static void *processDir(char *path)
+{
+    DIR *dirPointer = opendir(path);
+    if (!dirPointer)
+    {
         die("opendir");
     }
 
     incStat(&stats.dirs);
     V(newDataSignal);
 
-    struct dirent* entry = NULL;
+    struct dirent *entry = NULL;
     errno = 0;
 
     entry = readdir(dirPointer);
 
-    while (entry != NULL) {
-        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+    while (entry != NULL)
+    {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+        {
             processEntry(path, entry);
         }
         errno = 0;
         entry = readdir(dirPointer);
     }
-    if (errno) {
+    if (errno)
+    {
         die("readdir");
     }
-    if (closedir(dirPointer) == -1) {
+    if (closedir(dirPointer) == -1)
+    {
         die("closedir");
     }
 
     return NULL;
 }
 
-static void* processEntry(char* path, struct dirent* entry) {
+static void *processEntry(char *path, struct dirent *entry)
+{
     struct stat buf;
     int pathLength = strlen(path);
     int pathPlusEntryLength = pathLength + strlen(entry->d_name) + 2;
-    char* pathPlusEntry = malloc(pathPlusEntryLength * sizeof(char));
-    if (pathPlusEntry == NULL) {
+    char *pathPlusEntry = malloc(pathPlusEntryLength * sizeof(char));
+    if (pathPlusEntry == NULL)
+    {
         die("malloc");
     }
 
@@ -208,27 +243,32 @@ static void* processEntry(char* path, struct dirent* entry) {
     strcat(pathPlusEntry, "/");
     strcat(pathPlusEntry, entry->d_name);
 
-    if (lstat(pathPlusEntry, &buf) == -1) {
+    if (lstat(pathPlusEntry, &buf) == -1)
+    {
         free(pathPlusEntry);
         die("lstat");
     }
 
-    if (S_ISDIR(buf.st_mode)) {
+    if (S_ISDIR(buf.st_mode))
+    {
         processDir(pathPlusEntry);
         printStatistics();
     }
 
-    if (S_ISREG(buf.st_mode)) {
+    if (S_ISREG(buf.st_mode))
+    {
         P(grepThreadsSem);
 
         pthread_t tid;
         errno = pthread_create(&tid, NULL, processFile, pathPlusEntry);
-        if (errno != 0) {
+        if (errno != 0)
+        {
             die("pthread_create");
         }
 
         errno = pthread_detach(tid);
-        if (errno != 0) {
+        if (errno != 0)
+        {
             die("pthread_detach");
         }
 
@@ -242,39 +282,55 @@ static void* processEntry(char* path, struct dirent* entry) {
     return NULL;
 }
 
-static void* processFile(void* path) {
-    char* filePath = (char*)path;
+static void *processFile(void *path)
+{
+    char *filePath = (char *)path;
 
     incStat(&stats.files);
     V(newDataSignal);
 
-    FILE* fh = fopen(filePath, "r");
-    if (!fh) {
+    bool has_hits= false;
+
+    FILE *fh = fopen(filePath, "r");
+    if (!fh)
+    {
         die("fopen");
     }
 
     char line[4096 + 2];
 
-    while (fgets(line, sizeof(line), fh)) {
+    while (fgets(line, sizeof(line), fh))
+    {
         incStat(&stats.lines);
+        V(newDataSignal);
 
-        if (strstr(line, search_string)) {
+        if (strstr(line, search_string))
+        {
             incStat(&stats.lineHits);
+            has_hits= true;
+            V(newDataSignal);
         }
 
-        V(newDataSignal);
         printStatistics();
     }
 
-    if (ferror(fh)) {
+    if (has_hits){
+        incStat(&stats.fileHits);
+        V(newDataSignal);
+    }
+
+    if (ferror(fh))
+    {
         die("fgets");
     }
 
-    if (fclose(fh) != 0) {
+    if (fclose(fh) != 0)
+    {
         die("fclose");
     }
 
     decStat(&stats.activeGrepThreads);
+    V(newDataSignal);
     printStatistics();
 
     return NULL;
